40f344505344a274c567d513675fc8a5725ba939
ivan schlagheck <ivansch05@gmail.com>
2022-01-14 15:19:46 +0100

    implementing context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./context/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,7 +11,7 @@ function App() {
   useEffect(() => {
     const storedUserLoggedInformation = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLoggedInformation === '1') {
+    if (storedUserLoggedInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
@@ -23,20 +24,23 @@ function App() {
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
-
-
   return (
-    <React.Fragment>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
+      }}
+    >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
